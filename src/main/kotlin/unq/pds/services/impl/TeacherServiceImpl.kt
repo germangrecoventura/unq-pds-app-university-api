c4b1fbe1dea8d57c19a3de14060d17caf9b39412
germangrecoventura <germangv1998@gmail.com>
2023-03-24 14:14:11 -0300

    Add validator email static

--- src/main/kotlin/unq/pds/services/impl/TeacherServiceImpl.kt
+++ src/main/kotlin/unq/pds/services/impl/TeacherServiceImpl.kt
@@ -6,7 +6,25 @@ import org.springframework.transaction.annotation.Transactional
 import unq.pds.model.Teacher
 import unq.pds.persistence.TeacherDAO
 import unq.pds.services.TeacherService
+import java.util.regex.Matcher
 import java.util.regex.Pattern
+
+class ValidatorMail {
+    companion object {
+        fun validator(email: String?): Matcher {
+            var pattern = Pattern.compile(
+                "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                        "\\@" +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                        "(" +
+                        "\\." +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                        ")+"
+            )
+            return pattern.matcher(email)
+        }
+    }
+}
 
 @Service
 @Transactional
@@ -14,16 +32,6 @@ open class TeacherServiceImpl : TeacherService {
 
     @Autowired
     private lateinit var teacherDAO: TeacherDAO
-
-    val EMAIL_ADDRESS_PATTERN: Pattern = Pattern.compile(
-        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-                "\\@" +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-                "(" +
-                "\\." +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-                ")+"
-    )
 
     override fun save(teacher: Teacher): Teacher {
         if (teacher.getFirstName().isNullOrBlank()) {
@@ -35,7 +43,7 @@ open class TeacherServiceImpl : TeacherService {
         if (teacher.getEmail().isNullOrBlank()) {
             throw Throwable("The email cannot be empty")
         }
-        if (!EMAIL_ADDRESS_PATTERN.matcher(teacher.getEmail()).matches()) {
+        if (!ValidatorMail.validator(teacher.getEmail()).matches()) {
             throw Throwable("The email given is not an email")
         }
         return teacherDAO.save(teacher)
@@ -48,6 +56,7 @@ open class TeacherServiceImpl : TeacherService {
     override fun count(): Int {
         return teacherDAO.count().toInt()
     }
+
     override fun clearTeachers() {
         teacherDAO.deleteAll()
     }
