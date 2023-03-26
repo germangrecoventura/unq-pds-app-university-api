8048b055aa9db9d86e98bb59bdf7c9659b302978
germangrecoventura <germangv1998@gmail.com>
2023-03-26 14:34:54 -0300

    Fix import

--- src/main/kotlin/unq/pds/service/TeacherServiceImpl.kt
+++ src/main/kotlin/unq/pds/service/TeacherServiceImpl.kt
@@ -3,7 +3,6 @@ package unq.pds.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import unq.pds.model.Student
 import unq.pds.model.Teacher
 import unq.pds.persistence.TeacherDAO
 import unq.pds.webservice.Validator
