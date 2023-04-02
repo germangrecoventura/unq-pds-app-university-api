abd5c77b45c32de2147f0cc538725fb85dc8635b
germangrecoventura <germangv1998@gmail.com>
2023-04-02 11:55:25 -0300

    Delete commented tests

--- src/test/kotlin/unq/pds/services/StudentServiceTest.kt
+++ src/test/kotlin/unq/pds/services/StudentServiceTest.kt
@@ -1,10 +1,11 @@
 package unq.pds.services
 
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import unq.pds.api.dtos.StudentCreateRequestDTO
-import unq.pds.model.Student
 import unq.pds.services.builder.BuilderStudentDTO.Companion.aStudentDTO
 import unq.pds.services.impl.StudentServiceImpl
 
@@ -97,7 +98,7 @@ class StudentServiceTest {
 
     @Test
     fun `should throw an exception if the lastname has any number`() {
-        var request =aStudentDTO().withLastName("Gr3c0").build()
+        var request = aStudentDTO().withLastName("Gr3c0").build()
         val thrown: RuntimeException =
             Assertions.assertThrows(RuntimeException::class.java) { studentService.save(request) }
 
@@ -109,14 +110,14 @@ class StudentServiceTest {
 
     @Test
     fun `should throw an exception if email is null`() {
-        var request =aStudentDTO().withEmail(null).build()
+        var request = aStudentDTO().withEmail(null).build()
 
         Assertions.assertThrows(RuntimeException::class.java) { studentService.save(request) }
     }
 
     @Test
     fun `should throw an exception if email is empty`() {
-        var request =aStudentDTO().withEmail("").build()
+        var request = aStudentDTO().withEmail("").build()
         val thrown: RuntimeException =
             Assertions.assertThrows(RuntimeException::class.java) { studentService.save(request) }
 
@@ -128,8 +129,8 @@ class StudentServiceTest {
 
     @Test
     fun `should throw an exception when the email is already registered`() {
-        var request1 =aStudentDTO().withEmail("repetido@gmail.com").build()
-        var request2 =aStudentDTO().withEmail("repetido@gmail.com").build()
+        var request1 = aStudentDTO().withEmail("repetido@gmail.com").build()
+        var request2 = aStudentDTO().withEmail("repetido@gmail.com").build()
 
         studentService.save(request1)
         val thrown: RuntimeException =
@@ -143,7 +144,7 @@ class StudentServiceTest {
 
     @Test
     fun `should throw an exception when the email is not valid`() {
-        var request =aStudentDTO().withEmail("juanPerezgmail.com").build()
+        var request = aStudentDTO().withEmail("juanPerezgmail.com").build()
         val thrown: RuntimeException =
             Assertions.assertThrows(RuntimeException::class.java) { studentService.save(request) }
 
@@ -156,95 +157,17 @@ class StudentServiceTest {
 
     @Test
     fun `should update student name when firstname is valid`() {
-        var request =aStudentDTO().withEmail("prueba@gmail.com").build()
+        var request = aStudentDTO().withEmail("prueba@gmail.com").build()
         var student = studentService.save(request)
         student.setFirstName("Juan")
         var studentUpdated = studentService.update(student)
         Assertions.assertTrue(studentUpdated.getFirstName() == student.getFirstName())
     }
 
-    /*
-        @Test
-        fun `should throw an exception when firstname is null`() {
-            var request =
-                StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                    .withEmail("prueba@gmail.com").build()
-            var student = studentService.save(request)
 
-            var studentToModify = Student(null, student.getLastName(), student.getEmail())
-            studentToModify.setId(student.getId())
-
-            val thrown: RuntimeException =
-                Assertions.assertThrows(RuntimeException::class.java, { studentService.update(studentToModify) })
-
-            Assertions.assertEquals(
-                "The firstname cannot be empty",
-                thrown.message
-            )
-
-        }
-
-
-    // TODO: DE ACA PARA ABAJO ROMPEN PORQUE EN REALIDAD SON TEST DE MODELO Y NO DE SERVICES
-    // TODO: ARREGLAR MAÑANA. TAMBIEN FALTA UN TEST DE ACTUALIZAR CUANDO NO SE ENCUENTRA EL ESTUDIANTE
-    // TODO: CONSULTAR SI LOS TESTS DEL UPDATE NO SE PUEDEN HACER YA QUE EXPLOTA EL MODELO
-
-
-
-        @Test
-        fun `should throw an exception when firstname is empty`() {
-            var request =
-                StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                    .withEmail("prueba@gmail.com").build()
-            var student = studentService.save(request)
-            student.setFirstName("")
-
-            val thrown: RuntimeException =
-                Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-            Assertions.assertEquals(
-                "The firstname cannot be empty",
-                thrown.message
-            )
-        }
-
-        @Test
-        fun `should throw an exception when firstname contains numbers`() {
-            var request =
-                StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                    .withEmail("prueba@gmail.com").build()
-            var student = studentService.save(request)
-            student.setFirstName("Jos3")
-
-            val thrown: RuntimeException =
-                Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-            Assertions.assertEquals(
-                "The firstname can not contain numbers",
-                thrown.message
-            )
-        }
-
-        @Test
-        fun `should throw an exception when firstname contains special character`() {
-            var request =
-                StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                    .withEmail("prueba@gmail.com").build()
-            var student = studentService.save(request)
-            student.setFirstName("Jos@")
-
-            val thrown: RuntimeException =
-                Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-            Assertions.assertEquals(
-                "The firstname can not contain special characters",
-                thrown.message
-            )
-        }
-*/
     @Test
     fun `should update student lastname when lastname is valid`() {
-        var request =aStudentDTO().withEmail("prueba@gmail.com").build()
+        var request = aStudentDTO().withEmail("prueba@gmail.com").build()
         var student = studentService.save(request)
         student.setLastName("Perez")
 
@@ -253,78 +176,9 @@ class StudentServiceTest {
         Assertions.assertTrue(studentUpdated.getLastName() == student.getLastName())
     }
 
-    /*
-            @Test
-            fun `should throw an exception when lastname is null`() {
-                var request =
-                    StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                        .withEmail("prueba@gmail.com").build()
-                var student = studentService.save(request)
-                student.setLastName(null)
-
-                val thrown: RuntimeException =
-                    Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-                Assertions.assertEquals(
-                    "The lastname cannot be empty",
-                    thrown.message
-                )
-            }
-
-            @Test
-            fun `should throw an exception when lastname is empty`() {
-                var request =
-                    StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                        .withEmail("prueba@gmail.com").build()
-                var student = studentService.save(request)
-                student.setLastName("")
-
-                val thrown: RuntimeException =
-                    Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-                Assertions.assertEquals(
-                    "The lastname cannot be empty",
-                    thrown.message
-                )
-            }
-
-            @Test
-            fun `should throw an exception when lastname contains numbers`() {
-                var request =
-                    StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                        .withEmail("prueba@gmail.com").build()
-                var student = studentService.save(request)
-                student.setFirstName("G3reco")
-
-                val thrown: RuntimeException =
-                    Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-                Assertions.assertEquals(
-                    "The firstname can not contain numbers",
-                    thrown.message
-                )
-            }
-
-            @Test
-            fun `should throw an exception when lastname contains special character`() {
-                var request =
-                    StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                        .withEmail("prueba@gmail.com").build()
-                var student = studentService.save(request)
-                student.setLastName("Gre@")
-
-                val thrown: RuntimeException =
-                    Assertions.assertThrows(RuntimeException::class.java, { studentService.update(student) })
-
-                Assertions.assertEquals(
-                    "The lastname can not contain special characters",
-                    thrown.message
-                )
-            }
-    */
     @Test
     fun `should update student email when email is valid`() {
-        var request =aStudentDTO().withEmail("prueba@gmail.com").build()
+        var request = aStudentDTO().withEmail("prueba@gmail.com").build()
         var student = studentService.save(request)
         student.setEmail("juanPerez@gmail.com")
         var studentUpdated = studentService.update(student)
@@ -332,27 +186,9 @@ class StudentServiceTest {
         Assertions.assertTrue(studentUpdated.getEmail() == student.getEmail())
     }
 
-    /*
-        @Test
-        fun `should not update the teacher if you do not have a valid email`() {
-            var request =
-                StudentCreateRequestDTO.BuilderStudentCreateDTO().withFirstName("German").withLastName("Greco")
-                    .withEmail("prueba@gmail.com").build()
-            var teacher = studentService.save(request)
-            teacher.setEmail("juanPerezgmail.com")
-            val thrown: RuntimeException =
-                Assertions.assertThrows(RuntimeException::class.java, { studentService.update(teacher) })
-            Assertions.assertEquals(
-                "The email is not valid",
-                thrown.message
-            )
-        }
-        */
-
-
     @Test
     fun `should not update the teacher if the email already exists`() {
-        var request =aStudentDTO().withEmail("prueba@gmail.com").build()
+        var request = aStudentDTO().withEmail("prueba@gmail.com").build()
         studentService.save(request)
         var request2 = aStudentDTO().withEmail("jose@gmail.com").build()
         var teacher2 = studentService.save(request2)
