615e787bd79e9d36c34505293bc1ed19cc6d5f43
l_ziege <lucasziegemann@gmail.com>
2023-04-13 13:42:55 -0300

    Add methods to add and remove students,teachers and groups from commission

--- src/main/kotlin/unq/pds/model/Commission.kt
+++ src/main/kotlin/unq/pds/model/Commission.kt
@@ -23,6 +23,36 @@ class Commission(
     @ManyToMany(fetch = FetchType.EAGER)
     var groupsStudents: MutableSet<Group> = mutableSetOf()
 
+    fun addStudent(student: Student) {
+        if (isMyStudent(student)) throw CloneNotSupportedException("The student is already in the commission")
+        students.add(student)
+    }
+
+    fun removeStudent(student: Student) {
+        if (!isMyStudent(student)) throw NoSuchElementException("The student is not in the commission")
+        students.remove(student)
+    }
+
+    fun addTeacher(teacher: Teacher) {
+        if (isMyTeacher(teacher)) throw CloneNotSupportedException("The teacher is already in the commission")
+        teachers.add(teacher)
+    }
+
+    fun removeTeacher(teacher: Teacher) {
+        if (!isMyTeacher(teacher)) throw NoSuchElementException("The teacher is not in the commission")
+        teachers.remove(teacher)
+    }
+
+    fun addGroup(group: Group) {
+        if (isMyGroup(group)) throw CloneNotSupportedException("The group is already in the commission")
+        groupsStudents.add(group)
+    }
+
+    fun removeGroup(group: Group) {
+        if (!isMyGroup(group)) throw NoSuchElementException("The group is not in the commission")
+        groupsStudents.remove(group)
+    }
+
     fun getYear() = year
 
     fun getFourMonthPeriod() = fourMonthPeriod
@@ -37,5 +67,23 @@ class Commission(
 
     private fun validateYear() {
         if (year < 2000) throw InvalidAttributeValueException("Year should be greater than or equal to 2000")
+    }
+
+    private fun isMyStudent(student: Student): Boolean {
+        return students.any { it.getEmail() == student.getEmail() }
+    }
+
+    private fun isMyTeacher(teacher: Teacher): Boolean {
+        return teachers.any { it.getEmail() == teacher.getEmail() }
+    }
+
+    private fun isMyGroup(group: Group): Boolean {
+        return groupsStudents.any { it.name == group.name &&
+                                    it.members.size == group.members.size
+                                    it.members.all { member -> isMemberOfTheGroup(member, group) } }
+    }
+
+    private fun isMemberOfTheGroup(member: Student, group: Group): Boolean {
+        return group.members.any { it.getEmail() == member.getEmail() }
     }
 }
