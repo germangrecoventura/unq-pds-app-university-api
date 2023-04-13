f5e3994fee82ea8941755c7f356926cb0409863e
l_ziege <lucasziegemann@gmail.com>
2023-04-13 14:00:30 -0300

    Add methods to add and remove students, teachers and groups from commission service

--- src/main/kotlin/unq/pds/services/CommissionService.kt
+++ src/main/kotlin/unq/pds/services/CommissionService.kt
@@ -7,6 +7,12 @@ interface CommissionService {
     fun update(commission: Commission): Commission
     fun read(commissionId: Long): Commission
     fun delete(commissionId: Long)
+    fun addStudent(commissionId: Long, studentId: Long): Commission
+    fun removeStudent(commissionId: Long, studentId: Long): Commission
+    fun addTeacher(commissionId: Long, teacherId: Long): Commission
+    fun removeTeacher(commissionId: Long, teacherId: Long): Commission
+    fun addGroup(commissionId: Long, groupId: Long): Commission
+    fun removeGroup(commissionId: Long, groupId: Long): Commission
     fun count(): Int
     fun clearCommissions()
 }
--- src/main/kotlin/unq/pds/services/impl/CommissionServiceImpl.kt
+++ src/main/kotlin/unq/pds/services/impl/CommissionServiceImpl.kt
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import unq.pds.model.Commission
 import unq.pds.persistence.CommissionDAO
-import unq.pds.services.CommissionService
-import unq.pds.services.MatterService
+import unq.pds.services.*
 
 @Service
 @Transactional
@@ -14,6 +13,9 @@ open class CommissionServiceImpl : CommissionService {
 
     @Autowired private lateinit var commissionDAO: CommissionDAO
     @Autowired private lateinit var matterService: MatterService
+    @Autowired private lateinit var studentService: StudentService
+    @Autowired private lateinit var teacherService: TeacherService
+    @Autowired private lateinit var groupService: GroupService
 
     override fun save(commission: Commission): Commission {
         matterService.save(commission.getMatter())
@@ -32,6 +34,54 @@ open class CommissionServiceImpl : CommissionService {
     override fun delete(commissionId: Long) {
         if (commissionDAO.existsById(commissionId)) commissionDAO.deleteById(commissionId)
          else throw NoSuchElementException("There is no commission with that id")
+    }
+
+    override fun addStudent(commissionId: Long, studentId: Long): Commission {
+        val commission = this.read(commissionId)
+        val student = studentService.findById(studentId)
+        commission.addStudent(student)
+
+        return this.update(commission)
+    }
+
+    override fun removeStudent(commissionId: Long, studentId: Long): Commission {
+        val commission = this.read(commissionId)
+        val student = studentService.findById(studentId)
+        commission.removeStudent(student)
+
+        return this.update(commission)
+    }
+
+    override fun addTeacher(commissionId: Long, teacherId: Long): Commission {
+        val commission = this.read(commissionId)
+        val teacher = teacherService.findById(teacherId)
+        commission.addTeacher(teacher)
+
+        return this.update(commission)
+    }
+
+    override fun removeTeacher(commissionId: Long, teacherId: Long): Commission {
+        val commission = this.read(commissionId)
+        val teacher = teacherService.findById(teacherId)
+        commission.removeTeacher(teacher)
+
+        return this.update(commission)
+    }
+
+    override fun addGroup(commissionId: Long, groupId: Long): Commission {
+        val commission = this.read(commissionId)
+        val group = groupService.read(groupId)
+        commission.addGroup(group)
+
+        return this.update(commission)
+    }
+
+    override fun removeGroup(commissionId: Long, groupId: Long): Commission {
+        val commission = this.read(commissionId)
+        val group = groupService.read(groupId)
+        commission.removeGroup(group)
+
+        return this.update(commission)
     }
 
     override fun count(): Int {
