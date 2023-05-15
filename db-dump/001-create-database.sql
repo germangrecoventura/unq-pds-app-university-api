3ea942537710e9539bcd163c3a5473bbc03da2d3
germangrecoventura <germangv1998@gmail.com>
2023-05-15 20:18:30 -0300

    Refactor docker compose

--- db-dump/001-create-database.sql
+++ db-dump/001-create-database.sql
@@ -283,7 +283,7 @@ CREATE TABLE `hibernate_sequence` (
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (1);
+INSERT INTO `hibernate_sequence` VALUES (5);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
