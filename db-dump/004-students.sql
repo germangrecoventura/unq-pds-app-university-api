df4e3b2c4b4fdc350235faf718d825a9f5823f88
l_ziege <lucasziegemann@gmail.com>
2023-05-15 22:10:16 -0300

    Update 004-students.sql

--- db-dump/004-students.sql
+++ db-dump/004-students.sql
@@ -1,6 +1,7 @@
 INSERT INTO student (id, first_name, last_name, email, password, owner_github, token_github)
 VALUES (1, "German", "Greco Ventura", "german@gmail.com",
-        "QVNm6Z3nmXAqTzQUDWrGgTGLoyVKPw+z+RZ4784R4MZi5E2OpjqR01ChmR2qTmgo", "germangrecoventura",
-        "D3CibYiVfiYHab+2/s6y2wRAfQ"),
-       (2, "Lucas", "Ziegemann", "lucas@gmail.com", "QVNm6Z3nmXAqTzQUDWrGgTGLoyVKPw+z+RZ4784R4MZi5E2OpjqR01ChmR2qTmgo",
+        "QVNm6Z3nmXAqTzQUDWrGgTGLoyVKPw+z+RZ4784R4MZi5E2OpjqR01ChmR2qTmgo",
+        "germangrecoventura", null),
+       (2, "Lucas", "Ziegemann", "lucas@gmail.com",
+        "QVNm6Z3nmXAqTzQUDWrGgTGLoyVKPw+z+RZ4784R4MZi5E2OpjqR01ChmR2qTmgo",
         "lziege", null);
