a74142f0d4f8a0996a08ed028e63cea1e4e64b0a
germangrecoventura <germangv1998@gmail.com>
2023-06-01 20:49:06 -0300

    Add environment variables docker

--- waitingDatabase.sh
+++ waitingDatabase.sh
@@ -1,6 +1,6 @@
 echo "Initiating the wait to complete the database"
 
-while ! nc -z mysqldb 3306 ; do sleep 1 ; done
+while ! nc -z ${MYSQL_HOST} ${MYSQL_PORT} ; do sleep 1 ; done
 
 echo "Finished waiting to complete database"
 
