de6d916070fbc30b8ae7c69ab73dc902762d8878
germangrecoventura <germangv1998@gmail.com>
2023-06-16 21:06:01 -0300

    Refactor dockerfile with gradle

--- waitingDatabase.sh
+++ waitingDatabase.sh
@@ -4,4 +4,4 @@ while ! nc -z ${MYSQL_HOST} ${MYSQL_PORT}; do sleep 1; done
 
 echo "Finished waiting to complete database"
 
-java -jar /app/app-universidad.jar --server.port=${PORT}
+java -jar /app-universidad.jar --server.port=${PORT}
