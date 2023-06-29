b5219216199d65e6d34440d923af71494ac8e2d0
germangrecoventura <germangv1998@gmail.com>
2023-06-29 20:09:40 -0300

    Fix Cors

--- src/main/kotlin/unq/pds/security/SecurityConfig.kt
+++ src/main/kotlin/unq/pds/security/SecurityConfig.kt
@@ -61,7 +61,7 @@ open class SecurityConfig {
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.addAllowedOrigin("http://localhost:3000")
+        config.addAllowedOrigin("*")
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
         source.registerCorsConfiguration("/**", config)
